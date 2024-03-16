4b1a618138ff74fdea4886e2980b12a291efde07
Spandcynd <spandcynd@ukr.net>
2024-03-16 20:51:29 +0300

    Services page. Made why-us section

--- src/pages/services/js/initSlick.js
+++ src/pages/services/js/initSlick.js
@@ -1,20 +1,43 @@
-$('.slick').slick({
-  slidesToShow: 3,
-  infinite: false,
-  responsive: [
-    {
-      breakpoint: 992 + 1,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 770 + 1,
-      settings: {
-        centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 1,
-      },
-    },
-  ],
+// function debounce(clbk, interval) {
+//   let prevCall = Date.now();
+//   return function () {
+//     const currentCall = Date.now();
+//     if (currentCall - prevCall > interval) {
+//       clbk();
+//     }
+//     prevCall = currentCall;
+//   };
+// }
+
+function debounce(clbk, timeout) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(clbk, timeout);
+  };
+}
+
+$(document).ready(function () {
+  $(window)
+    .on(
+      'resize',
+      debounce(function () {
+        const slick = $('.slick');
+        if ($(window).width() <= 1200) {
+          if (!slick.hasClass('slick-initialized')) {
+            slick.slick({
+              infinite: false,
+              slidesToShow: 1,
+              dots: true,
+              draggable: false,
+            });
+          }
+        } else {
+          if (slick.hasClass('slick-initialized')) {
+            slick.slick('unslick');
+          }
+        }
+      }, 100),
+    )
+    .trigger('resize');
 });
